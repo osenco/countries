47064db2171db06c38337fb66dd061d7a06f0f3a
Mauko Maunde <maukoese@gmail.com>
2018-08-13 16:01:23 +0300

    Create counties.php

---
+++ kenya/counties.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package Halvetica Nua
+ * @subpackage Functions
+ * @version 1.8
+ * @since 1.8
+ * @author Osen Concepts < hi@osen.coke >
+ * @link https://wpthemes.osen.co.ke/nua
+ */
